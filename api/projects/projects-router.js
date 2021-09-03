a3a533e58be5778fe93fc49ee918f55033d83b2e
Bradly <bradlylewisdev@gmail.com>
2021-09-03 07:25:10 -0400

    wip

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,33 @@
-// Write your "projects" router here!
+const express = require('express')
+const Project = require('./projects-model')
+const router = express.Router()
+
+router.get('/', (req, res, next) => {
+    Project.get()
+        .then(projects => {
+            res.json(projects)
+        })
+        .catch(next)
+})
+router.get('/:id', (req, res) => {
+    console.log('hello')
+
+})
+router.post('/', (req, res) => {
+    console.log('hello')
+
+})
+router.put('/:id', (req, res) => {
+    console.log('hello')
+
+})
+router.delete('/:id', (req, res) => {
+    console.log('hello')
+
+})
+router.get(':id/actions', (req, res) => {
+    console.log('hello')
+
+})
+
+module.exports = router
